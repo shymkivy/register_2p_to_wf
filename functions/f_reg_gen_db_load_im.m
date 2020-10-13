925362a3e7ed6c0bf1aa05d0d257af99ad971f75
Yuriy Shymkiv <shymkivy@gmail.com>
2020-10-12 23:40:56 -0400

    updates

---
+++ functions/f_reg_gen_db_load_im.m
@@ -0,0 +1,28 @@
+function im_out = f_reg_gen_db_load_im(im_path)
+
+all_files = dir([im_path '*']);
+all_files2 = {all_files.name};
+
+all_ext = cell(numel(all_files2),1);
+for n_fil = 1:numel(all_files2)
+    [~,~,all_ext{n_fil}] = fileparts(all_files2{n_fil});
+end
+
+[f_dir] = fileparts(im_path);
+
+if sum(strcmpi(all_ext, '.tif'))
+    files1 = all_files2(strcmpi(all_ext, '.tif'));
+    image1 = double(imread([f_dir '\' files1{1}]));
+elseif sum(strcmpi(all_ext, '.tiff'))
+    files1 = all_files2(strcmpi(all_ext, '.tiff'));
+    image1 = double(imread([f_dir '\' files1{1}]));
+elseif sum(strcmpi(all_ext, '.fig'))
+    files1 = all_files2(strcmpi(all_ext, '.fig'));
+    fig1 = open([f_dir '\' files1{1}]);
+    image1 = fig1.Children.Children.CData;
+    close(fig1);
+end
+image1 = image1 - min(image1(:));
+im_out = image1/max(image1(:));
+
+end
---
+++ generate_image_DB2.m
@@ -0,0 +1,76 @@
+clear;
+close all;
+
+addpath('C:\Users\rylab_dataPC\Desktop\Yuriy\register_2p_to_wf');
+addpath('C:\Users\rylab_dataPC\Desktop\Yuriy\register_2p_to_wf\functions');
+
+load_data_path = 'F:\data\Auditory\mapping_2p_reg_data';
+save_data_path = 'C:\Users\rylab_dataPC\Desktop\Yuriy\register_2p_to_wf';
+
+
+mouse_tag_col_name = 'mouse_tag';
+area_tag_col_name = 'area';
+wf_fnames_col_name = 'mapping_wf_frame';
+twop_fnames_col_name = {'mapping_reg_2p_surface', 'mapping_reg_2p_fov'};
+
+%%
+AC_data = readtable('C:\Users\rylab_dataPC\Desktop\Yuriy\AC_2p_analysis\AC_data_list.xlsx');
+AC_data = AC_data(~isnan(AC_data.im_use_dset),:);
+
+
+%%
+mouse_tags = unique(AC_data.(mouse_tag_col_name));
+num_mice = numel(mouse_tags);
+%% sort mouse tags
+date_mice = zeros(num_mice,3);
+for n_ms = 1:num_mice
+    indx = strfind(mouse_tags{n_ms}, '_');
+    date_mice(n_ms,1) = str2double(mouse_tags{n_ms}(indx(2)+1:indx(2)+2));
+    date_mice(n_ms,2) = str2double(mouse_tags{n_ms}(1:indx(1)-1));
+    date_mice(n_ms,3) = str2double(mouse_tags{n_ms}(indx(1)+1:indx(2)-1));
+end
+[~, sind1] = sort(date_mice(:,3), 'ascend');
+[~, sind2] = sort(date_mice(sind1,2), 'ascend');
+[~, sind3] = sort(date_mice(sind1(sind2),1), 'ascend');
+
+mouse_tags = mouse_tags(sind1(sind2(sind3)));
+
+%%
+data_all = struct();
+for n_ms = 1:num_mice
+    data_all(n_ms).mouse_num = n_ms;
+    AC_data2 = AC_data(strcmpi(AC_data.mouse_tag, mouse_tags{n_ms}),:);
+    wf_fname = unique(AC_data2.(wf_fnames_col_name));
+    wf_fname = wf_fname(~strcmpi(wf_fname, ''));
+    data_all(n_ms).wf_fname = wf_fname(1);
+    % make regions
+    regions1 = unique(AC_data2.(area_tag_col_name));
+    data_all(n_ms).wf_im = {f_reg_gen_db_load_im([load_data_path, '\', mouse_tags{n_ms}, '\', wf_fname{1}])};
+    empty_reg = false(numel(regions1),1);
+    for n_reg = 1:numel(regions1)
+        data_all(n_ms).regions(n_reg).region_name = regions1(n_reg);
+        AC_data3 = AC_data2(strcmpi(AC_data2.(area_tag_col_name), regions1{n_reg}),:);
+        data_all(n_ms).regions(n_reg).fov_fname = cell(numel(twop_fnames_col_name),1);
+        data_all(n_ms).regions(n_reg).fov_im = cell(numel(twop_fnames_col_name),1);
+        empty_fov_fname = true(numel(twop_fnames_col_name),1);
+        for n_fov = 1:numel(twop_fnames_col_name)
+            fov1 = unique(AC_data3.(twop_fnames_col_name{n_fov}));
+            fov1 = fov1(~strcmpi(fov1, ''));
+            if ~isempty(fov1)
+                data_all(n_ms).regions(n_reg).fov_fname{n_fov} = fov1{1};
+                data_all(n_ms).regions(n_reg).fov_im{n_fov} = f_reg_gen_db_load_im([load_data_path, '\', mouse_tags{n_ms}, '\', fov1{1}]);
+                empty_fov_fname(n_fov) = 0;
+            end
+        end
+        data_all(n_ms).regions(n_reg).fov_fname(empty_fov_fname) = [];
+        data_all(n_ms).regions(n_reg).fov_im(empty_fov_fname) = [];
+        empty_reg(n_reg) = prod(empty_fov_fname);
+        data_all(n_ms).regions(n_reg).regions_tforms = [];
+        data_all(n_ms).regions(n_reg).current_tform = '0';
+    end
+    data_all(n_ms).regions(empty_reg) = [];
+end
+
+%% save 
+save([save_data_path '\reg_images_db.mat'], 'data_all');
+
