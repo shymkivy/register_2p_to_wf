efc6aa376c5e0f7a739b82195fb7614e50a3746a
yuriy <shymkivy@gmail.com>
2021-01-16 13:22:31 -0500

    mapping registration

--- functions/f_reg_align_point_save.m
+++ functions/f_reg_align_point_save.m
@@ -5,7 +5,11 @@ if ~isempty(app.map_pt)
     
     for n_pt = 1:numel(app.map_pt)
         if app.data_all(n_mouse).wf_mapping_regions{app.current_mapping_plot,n_pt}
-            app.data_all(n_mouse).wf_mapping_regions_coords{app.current_mapping_plot,n_pt} = app.map_pt{n_pt}.Position;
+            if isempty(app.map_pt{n_pt})
+                app.data_all(n_mouse).wf_mapping_regions_coords{app.current_mapping_plot,n_pt} = [];
+            else
+                app.data_all(n_mouse).wf_mapping_regions_coords{app.current_mapping_plot,n_pt} = app.map_pt{n_pt}.Position;
+            end
         end
     end
 end
